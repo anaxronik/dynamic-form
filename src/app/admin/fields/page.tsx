1b6ca5cd742f0410b382e6fcd8204831d55ae775
Sergey Komarov <anaxronik@gmail.com>
2024-11-27 01:03:18 +0400

    connect prisma

--- src/app/admin/fields/page.tsx
+++ src/app/admin/fields/page.tsx
@@ -1,7 +1,25 @@
-export default function Fields() {
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Администрирование полей",
+  description: "Создание и редактирование полей",
+};
+
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+export default async function Fields() {
+  const items = await prisma.formField.findMany();
+
   return (
-    <div className="flex gap-3">
+    <div>
       <div>Fields</div>
+      <div>
+        {items?.map((item) => (
+          <div key={item.id}>{item.name}</div>
+        ))}
+      </div>
     </div>
   );
 }
